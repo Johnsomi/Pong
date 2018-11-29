21ba8d793ba072d68d3fc4a6f46a49ec0e679e6b
Johnsomi <mj4100@students.inghamisd.org>
2018-11-29 13:29:16 -0500

    gfhryjtd

---
+++ pong/pong/Sprites/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace pong.Sprites
+{
+    public class Ball : Sprite
+    {
+        private float _timer = 0f; // incrementing the speed over time
+        private Vector2? _startPosition = null; // "?" means it's nullable
+        private float? _startSpeed;
+        private bool _isPlaying;
+
+        public Score Score;
+        public int SpeedIncrementSpan = 10; // how often the speed will increment
+
+        public Ball(Texture2D texture) 
+            : base(texture)
+        {
+            Speed = 3f;
+        }
+
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            if(_startPosition == null)
+            {
+                _startPosition = Position;
+                _startSpeed = Speed;
+            }
+        }
+    }
+}
