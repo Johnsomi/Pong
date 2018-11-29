424e030de0eedca28a7016ea9962ca7794854e55
Johnsomi <mj4100@students.inghamisd.org>
2018-11-29 13:52:26 -0500

    Update Ball.cs
    18 minutes in

--- pong/pong/Sprites/Ball.cs
+++ pong/pong/Sprites/Ball.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace pong.Sprites
 {
@@ -30,7 +31,67 @@ namespace pong.Sprites
             {
                 _startPosition = Position;
                 _startSpeed = Speed;
+
+                Restart();
             }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+                _isPlaying = true;
+
+            if (!_isPlaying)
+                return;
+
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer > SpeedIncrementSpan)
+            {
+                Speed++;
+                _timer = 0;
+            }
+
+            foreach (var sprite in sprites)
+            {
+                if (sprite == this)
+                    continue;
+
+                if (this.Velocity.X > 0 && this.IsTouchingLeft(sprite))
+                    this.Velocity.X = -this.Velocity.X;
+                if (this.Velocity.X < 0 && this.IsTouchingRight(sprite))
+                    this.Velocity.X = -this.Velocity.X;
+
+                if (this.Velocity.Y > 0 && this.IsTouchingTop(sprite))
+                    this.Velocity.Y = -this.Velocity.Y;
+                if (this.Velocity.Y < 0 && this.IsTouchingBottom(sprite))
+                    this.Velocity.Y = -this.Velocity.Y;
+            }
+
+            //__________
+        }
+
+        public void Restart()
+        {
+            var direction = Game1.Random.Next(0, 4);
+
+            switch (direction)
+            {
+                case 0:
+                    Velocity = new Vector2(1, 1);
+                    break;
+                case 1:
+                    Velocity = new Vector2(1, -1);
+                    break;
+                case 2:
+                    Velocity = new Vector2(-1, -1);
+                    break;
+                case 3:
+                    Velocity = new Vector2(-1, 1);
+                    break;
+            }
+
+            Position = (Vector2)_startPosition;
+            Speed = (float)_startSpeed;
+            _timer = 0;
+            _isPlaying = false;
         }
     }
 }
